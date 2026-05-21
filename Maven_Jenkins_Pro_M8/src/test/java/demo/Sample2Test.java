19691c3b5ea306ec9462abda4bdd5e07c94165d4
Tazeen Iffat <41568669+9955224870@users.noreply.github.com>
2026-05-21 16:06:55 +0530

    Update Sample2Test.java

--- Maven_Jenkins_Pro_M8/src/test/java/demo/Sample2Test.java
+++ Maven_Jenkins_Pro_M8/src/test/java/demo/Sample2Test.java
@@ -8,13 +8,8 @@ public class Sample2Test {
 	public void case123() {
 		System.out.println("sample test123");
 	}
-	
-	
 	@Test
 	public void case112233() {
 		System.out.println("sample test112233");
 	}
-	
-
-
 }
